f0dd1546c438da3832b51f419373411a93b7b98f
morngrar <sveinkare@gmail.com>
2014-02-03 19:14:00 +0100

    Working GUI added with glade file

--- peditor.py
+++ peditor.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python
 
-print("Hello world")
+from gi.repository import Gtk, GtkSource, GObject
+from os.path import abspath, dirname, join
+
+WHERE_AM_I = abspath(dirname(__file__))
+
+class MyApp(object):
+
+	def __init__(self):
+		self.builder = Gtk.Builder()
+		self.glade_file = join(WHERE_AM_I, "UI.glade")
+		GObject.type_register(GtkSource.View)
+		self.builder.add_from_file(self.glade_file)
+		self.main_window = self.builder.get_object("window1")
+		
+
+
+if __name__ == "__main__":
+	try:
+		gui = MyApp()
+		gui.main_window.show_all()
+		Gtk.main()
+	except KeyboardInterrupt:
+		pass
